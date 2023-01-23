2bcc602a264b7f7923019a0ee8a4f94bb6e0f68b
Arpan Agrawal <arpan2@illinois.edu>
2023-01-23 13:12:08 -0500

    Trying to call proverbot via OS call, the 'dune build' command gets stuck at 97%.

--- theories/Test.v
+++ theories/Test.v
@@ -1,32 +1,5 @@
 Require Import SynthesisPlugin.
 
-Fail ExploreProof.
-
-Theorem bar:
-  forall (T : Set) (t : T), T.
-Proof.
-  ExploreProof. my_intro T. ExploreProof. my_intro t. ExploreProof. apply t.
-Qed.
-
-Fail NameProof.
-
-Fail TryThis.
-
-Theorem foo:
-    forall (T: Set) (t: T), T.
-Proof.
-    (* NameProof. my_intro T. *)
-    NameProof.
-Abort.
-
-Theorem fiff: (2 + 2 = 5) <-> (2 + 2 = 3).
-Proof.
-  TryThis.
-  intros.
-  split.
-  TryThis.
-Abort.
-
 Theorem fiff_2 : (2 + 2 = 5) <-> (2 + 2 = 3).
 Proof.
   PrintGoals.
