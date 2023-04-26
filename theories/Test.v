60d6e93b786929079a28feb0536e20c1423021ce
Arpan Agrawal <arpan2@illinois.edu>
2023-04-26 12:27:41 -0500

    Added code to run alectryon and generate a d3 tree.

--- theories/Test.v
+++ theories/Test.v
@@ -1,12 +1,10 @@
 Require Import SynthesisPlugin.
 
+Set Proverbot Path "/home/arpan/Downloads/proverbot9001-plugin/".
+
+Set Current File Path "/home/arpan/Downloads/coq-synthesis/theories/Test.v".
+
 Theorem fiff_2 : (2 + 2 = 5) -> (2 + 2 = 3) -> (2 + 2 = 7) <-> False.
 Proof.
-  (* RunProverbotOnFile (IDENT "~/Downloads/coq-synthesis/theories/Test.v"). *)
-  (* StringInput abc. *)
-  (* split.  *)
-  (* - PredictTactic. *)
   RunProverbot.
 Admitted.
-
-(* Decompile fiff_2. *)
