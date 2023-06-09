be2e5ec4ed872f75068f9396fea1c85ad7988998
Arpan Agrawal <arpan2@illinois.edu>
2023-06-09 11:57:30 -0500

    Removed redundant ml and mlg files.

--- src/simple_declare.ml
+++
@@ -1,4 +0,0 @@
-let declare_definition ~poly name sigma body =
-  let cinfo = Declare.CInfo.make ~name ~typ:None () in
-  let info = Declare.Info.make ~poly () in
-  Declare.declare_definition ~info ~cinfo ~opaque:false ~body sigma
